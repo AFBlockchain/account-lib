ba4741bfe5581b43f0d6829e532ce5dd3958a496
McXD <fyl155165@gmail.com>
2022-01-05 16:52:45 +0800

    Define cordapp user management flows

--- workflows/src/main/kotlin/hk/edu/polyu/af/bc/account/flows/CordappUserFlows.kt
+++ workflows/src/main/kotlin/hk/edu/polyu/af/bc/account/flows/CordappUserFlows.kt
@@ -9,10 +9,14 @@ import net.corda.core.flows.FlowLogic
 import net.corda.core.identity.Party
 
 /// This package contains workflows for node-layer CordappUser management
+/// parallel with [UserDetailsManager] from Spring Security (https://docs.spring.io/spring-security/site/docs/current/api/org/springframework/security/provisioning/UserDetailsManager.html)
+/// No need to manage credentials since this is only carried out at the rpc side
 
 /**
  * Create an [AccountInfo], and based on which populate the given [CordappUser]. The account created can be proactively
  * shared with other nodes on the network. This can be done via a configuration file.
+ *
+ * The created [CordappUser] *should* be visible for the entire business network.
  */
 class CreateCordappUser(private val cordappUser: CordappUser): FlowLogic<Unit>() {
     @Suspendable
@@ -42,5 +46,28 @@ class ShareCordappUser(private val cordappUser: CordappUser,
         subFlow(ShareAccountInfo(accountInfoStateAndRef, recipients))
 
         logger.info("CordappUser shared with $recipients: $cordappUser")
+    }
+}
+
+/**
+ * Fetch information about a [CordappUser] at the node level. The returned instance is of an anonymous class, which severs
+ * as a container for node-level user information.
+ */
+class GetCordappUserByName(private val accountName: String): FlowLogic<CordappUser?>() {
+    override fun call(): CordappUser? {
+        TODO("Not yet implemented")
+    }
+}
+
+/**
+ * The behaviour of this check is consistent with whether [CreateCordappUser] can successfully create the user. If this
+ * flow returns `true`, then a [CordappUser] can be created.
+ *
+ * The recommended practice is to assume an *Application Network* within which the account name is unique. The network
+ * can be defined either hard-coded or with a configuration file.
+ */
+class CheckCordappUserExistence(private val accountName: String): FlowLogic<Boolean>() {
+    override fun call(): Boolean {
+        TODO("Not yet implemented")
     }
 }
