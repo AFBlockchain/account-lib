6d30ccd1255d87935e2fcc43d1a1323afade9504
McXD <fyl155165@gmail.com>
2022-01-08 14:59:28 +0800

    Add user mgnt flows

--- workflows/src/main/kotlin/hk/edu/polyu/af/bc/account/flows/user/UserManagementFlows.kt
+++ workflows/src/main/kotlin/hk/edu/polyu/af/bc/account/flows/user/UserManagementFlows.kt
@@ -1,6 +1,7 @@
 package hk.edu.polyu.af.bc.account.flows.user
 
 import net.corda.core.flows.FlowLogic
+import java.util.*
 
 // Representation of a user is just a String. So there's no need for "reading" and "updating" a user
 // The meaning of "deleting" a user is not clear yet.
@@ -8,7 +9,7 @@ import net.corda.core.flows.FlowLogic
 /**
  * Create a user in the current identity plane.
  */
-class CreateUser(val name: String): FlowLogic<Unit>() {
+class CreateUser(val username: String): FlowLogic<Unit>() {
     override fun call() {
         TODO("Not yet implemented")
     }
@@ -17,8 +18,26 @@ class CreateUser(val name: String): FlowLogic<Unit>() {
 /**
  * Check whether the user given exists in the current identity plane.
  */
-class IsUserExists(val name: String): FlowLogic<Boolean>() {
+class IsUserExists(val username: String): FlowLogic<Boolean>() {
     override fun call(): Boolean {
+        TODO("Not yet implemented")
+    }
+}
+
+/**
+ * Return all users in the current identity plane
+ */
+class AllUsers(): FlowLogic<List<String>>() {
+    override fun call(): List<String> {
+        TODO("Not yet implemented")
+    }
+}
+
+/**
+ * Get the underlying [UUID] for this user. Primarily, this identifier is used for vault query.
+ */
+class GetUserUUID(val username: String): FlowLogic<UUID>() {
+    override fun call(): UUID {
         TODO("Not yet implemented")
     }
 }
