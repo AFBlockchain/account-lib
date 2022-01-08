0cc1ad2e3d3fa500a125057e26fe44b22899abfa
YANGZhaochen2002 <93120280+YANGZhaochen2002@users.noreply.github.com>
2022-01-08 19:29:42 +0800

    Update NetworkIdentityPlaneContextFlows.kt

--- workflows/src/main/kotlin/hk/edu/polyu/af/bc/account/flows/plane/NetworkIdentityPlaneContextFlows.kt
+++ workflows/src/main/kotlin/hk/edu/polyu/af/bc/account/flows/plane/NetworkIdentityPlaneContextFlows.kt
@@ -1,6 +1,8 @@
-package hk.edu.polyu.af.bc.account.flows.plane
+package hk.edu.polyu. af.bc.account.flows.plane
 
+import hk.edu.polyu.af.bc.account.flows.plane.NetworkIdentityPlaneContext.Companion.currentPlane
 import hk.edu.polyu.af.bc.account.states.NetworkIdentityPlane
+import net.corda.core.flows.FlowException
 import net.corda.core.flows.FlowLogic
 
 /// Flows to manage the current NetworkIdentityPlane at the corda node
@@ -10,15 +12,22 @@ import net.corda.core.flows.FlowLogic
  */
 class GetCurrentNetworkIdentityPlane(): FlowLogic<NetworkIdentityPlane?>() {
     override fun call(): NetworkIdentityPlane? {
-        TODO("Not yet implemented")
+        if(currentPlane==null){
+            throw FlowException("currentPlane is null")
+        }
+
+        return currentPlane
     }
 }
 
 /**
  * Set the current [NetworkIdentityPlaneContext] to hold the given plane
  */
-class SetCurrentNetworkIdentityPlane(val plane: NetworkIdentityPlane): FlowLogic<NetworkIdentityPlane>() {
-    override fun call(): NetworkIdentityPlane {
-        TODO("Not yet implemented")
+class SetCurrentNetworkIdentityPlane(val plane: NetworkIdentityPlane): FlowLogic<Unit>() {
+    override fun call(): Unit {
+        if(currentPlane==null){
+            throw FlowException("currentPlane is null")
+        }
+        NetworkIdentityPlaneContext.currentPlane=plane
     }
 }
